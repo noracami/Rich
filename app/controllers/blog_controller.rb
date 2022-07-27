b78d3dd285c5e165384cec78b7c1dfc911a11e1c
noracami <kerke.2011@gmail.com>
2022-07-27 18:14:47 +0800

    form, post, authenticity_token

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,6 +1,11 @@
 class BlogController < ApplicationController
   def index
   end
-  def new_post
+  def new
+  end
+  def create
+    # 寫入資料庫
+    redirect_to "/blog"
+    # render html: "已成功新增文章"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/blog', to: "blog#index"
-  get '/blog/new_post', to: "blog#new_post"
+  get '/blog/new', to: "blog#new"
+  post '/blog/', to: "blog#create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get("/", {to: "welcome#home"})
   get "/", to: "welcome#home"
