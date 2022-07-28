486d58d36950d7290bfef85d068511f524433a9e
noracami <kerke.2011@gmail.com>
2022-07-28 10:57:39 +0800

    REST

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get '/blog', to: "blog#index"
-  get '/blog/new', to: "blog#new"
-  post '/blog/', to: "blog#create"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # get("/", {to: "welcome#home"})
   get "/", to: "welcome#home"
+
   get "/about", to: "welcome#about"
+
+  get '/blog', to: "blog#index"
+  post '/blog/', to: "blog#create"
+  get '/blog/new', to: "blog#new"
+  get '/blog/:id', to: "blog#show"
+  get '/blog/:id/edit', to: "blog#edit"
+  patch '/blog/:id', to: "blog#update"
+  delete '/blog/:id', to: "blog#destory"
 end
