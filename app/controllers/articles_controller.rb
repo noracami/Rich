5db8c213463f17e8b080ed95abfee85f0d3eabd4
noracami <kerke.2011@gmail.com>
2022-08-01 15:30:20 +0800

    rescue

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
   def create
     @article = Article.new(article_params)
 
@@ -14,15 +16,22 @@ class ArticlesController < ApplicationController
     # Article.find_by(id: 3) # 可查詢不同欄位, 查不到 > nil
     # render html: Article.where(id: 5).size
     # render html: Article.find_by!(params[:id]).title
-    @article = Article.find_by!(params[:id])
+    # begin
+      # Error 錯誤
+      # Exception 例外
+      # @article = Article.find(params[:id])
+    # rescue
+      # redirect_to blogs_path
+    # end
+    @article = Article.find_by!(id: params[:id])
   end
 
   def edit
-    @article = Article.find_by!(params[:id])
+    @article = Article.find_by!(id: params[:id])
   end
 
   def update
-    @article = Article.find_by!(params[:id])
+    @article = Article.find_by!(id: params[:id])
 
     if @article.update(article_params)
       redirect_to blogs_path
@@ -32,8 +41,12 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find_by!(params[:id])
+    @article = Article.find_by!(id: params[:id])
     @article.destroy
+    redirect_to blogs_path
+  end
+
+  def record_not_found
     redirect_to blogs_path
   end
 
