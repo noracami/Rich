056aa319aeb4a1c337343d44c7112620d3677b2e
noracami <kerke.2011@gmail.com>
2022-08-04 12:52:24 +0800

    login helper

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,10 +1,15 @@
 class BlogsController < ApplicationController
+  include UsersHelper
   def index
     # flash[:notice] = 'test'
     @articles = Article.all
   end
 
   def new
-    @article = Article.new
+    if user_singed_in?
+      @article = Article.new
+    else
+      redirect_to sign_in_users_path
+    end
   end
 end
