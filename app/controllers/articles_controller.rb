da1b9a544749bd74ecf110b9023e5ab6975be40a
noracami <kerke.2011@gmail.com>
2022-08-08 15:04:30 +0800

    unlock article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
-  before_action :authenticate_user!, expect: [:show]
-  before_action :find_article, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:show, :unlock]
+  before_action :find_current_user_article, only: [:edit, :update, :destroy]
+  before_action :find_article, only: [:show, :unlock]
 
   def create
     @article = current_user.articles.new(article_params)
@@ -13,7 +14,6 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find_by!(id: params[:id])
   end
 
   def edit
@@ -32,13 +32,35 @@ class ArticlesController < ApplicationController
     redirect_to blogs_path, notice: '文章刪除成功'
   end
 
+  def unlock
+    if @article.pincode == params[:pincode]
+      set_unlock_article(@article.id)
+      redirect_to article_path(@article)
+    else
+      redirect_to article_path(@article), notice: "pincode error!!"
+    end
+  end
+
   private
 
   def article_params
     params.require(:article).permit(:title, :content, :pincode)
   end
 
-  def find_article
+  def find_current_user_article
     @article = current_user.articles.find_by!(id: params[:id])
+  end
+
+  def find_article
+    @article = Article.find_by!(id: params[:id])
+  end
+
+  def set_unlock_article(article_id)
+    if session[:unlock_articles]
+      session[:unlock_articles] << article_id
+      session[:unlock_articles].uniq!
+    else
+      session[:unlock_articles] = [article_id]
+    end
   end
 end
