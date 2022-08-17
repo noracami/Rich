6539c1d3210e1d7a6d5828e569b127cd20644ca9
ker ker <kerke.2011@gmail.com>
2022-08-17 17:11:02 +0800

    建立訂單

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,26 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
   
   def new
     @order = Order.new
   end
 
   def create
-    redirect_to new_order_path, notice: "iii"
+    order = current_user.orders.new(order_params)
+    order.price = 10
+    p order
+
+    if order.save
+      redirect_to '/', notice: '建立訂單成功'
+      # 刷卡
+    else
+      redirect_to plans_path, notice: '系統忙碌中，請稍後再試'
+    end
   end
 
-  def show
+  private
+
+  def order_params
+    params.require(:order).permit(:note)
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,35 +1,46 @@
 class Order < ApplicationRecord
   include AASM
 
+  before_create :create_serial
+
   # relationships
   belongs_to :user
 
   # validation
-  validates :serial, presence: true
+  # validates :serial, presence: true
   validates :price, presence: true
   validates :state, presence: true
 
   aasm(column: 'state', no_direct_assignment: true) do
     state :pending, initial: true
-    state :paid, :refunded, :cancelled, :fail
+    state :paid, :refunded, :cancelled, :failed
 
     event :pay do
-      transitions from: %i[ pending fail ], to: :paid
+      transitions from: %i[ pending failed ], to: :paid
       after do
         puts "text"
       end
     end
 
     event :fail do
-      transitions from: :pending, to: :fail
+      transitions from: :pending, to: :failed
     end
 
     event :cancel do
-      transitions from: [:pending, :fail], to: :cancelled
+      transitions from: [:pending, :failed], to: :cancelled
     end
 
     event :refund do
       transitions from: :paid, to: :refunded
     end
+  end
+
+  private
+
+  def create_serial
+    # "ORD20220815XXXXXX(0~9a-zA-Z)"
+    rand_str = [*(0..9), *('a'..'z'), *('A'..'Z')].sample(6).join
+    now = Time.now
+    self.serial = "ORD%4d%02d%02d%s" % [now.year, now.month, now.day, rand_str]
   end
 end
