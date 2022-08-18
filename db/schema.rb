c6e5e927454d40dc39f3505ee13fd474c881d639
ker ker <kerke.2011@gmail.com>
2022-08-18 20:37:10 +0800

    WIP add role

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_18_064619) do
+ActiveRecord::Schema.define(version: 2022_08_18_084711) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -114,6 +114,7 @@ ActiveRecord::Schema.define(version: 2022_08_18_064619) do
     t.date "birthday"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "role", default: "user"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
